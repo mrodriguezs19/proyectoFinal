19078291d62c396e51a044811576e1e299c3f0e1
mrodriguezs19 <michael198rs@gmail.com>
2020-04-21 19:03:05 +0200

    Angular , primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+//Componentes
+import { AppComponent } from "./app.component";
+import { PaginaPrincipalComponent } from "./pagina-principal/pagina-principal.component";
+import { NoEncontradoComponent } from "./no-encontrado/no-encontrado.component";
+import { RegistroComponent } from "./registro/registro.component";
+import { FuncionamientoComponent } from './funcionamiento/funcionamiento.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
+
+
+const appRoutes: Routes = [
+  { path: "", component: PaginaPrincipalComponent },
+  { path: "funcionamiento", component: FuncionamientoComponent},
+  { path: "registro", component: RegistroComponent},
+  { path: "login", component: LoginComponent},
+  { path: "**", component: NoEncontradoComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaPrincipalComponent,
+    NoEncontradoComponent,
+    RegistroComponent,
+    FuncionamientoComponent,
+    LoginComponent,
+    NavbarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule,
+    ReactiveFormsModule,RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
